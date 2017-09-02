cf7f0c5613a584db2296b29d57b332cb59f5206c
abster <rajakarunanayake@gmail.com>
2017-09-02 16:54:55 +1000

    validation

--- src/main/java/au/com/dius/store/app/AppRunner.java
+++ src/main/java/au/com/dius/store/app/AppRunner.java
@@ -24,57 +24,57 @@ import au.com.dius.store.product.VGAAdapter;
  */
 
 public class AppRunner {
-	
+
 	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Invalid Usage. At least one SKU must be provided.");
+			System.exit(1);
+		}
+		
 		CheckoutApp app = new CheckoutApp();
 		app.loadProductData();
 		app.checkout(args[0]);
 	}
-	
-	
-	
-	
 
 }
 
 class CheckoutApp {
 	private Checkout co;
 	private PricingRulesEngine pricingRules;
-	
-	
+
 	public void checkout(String args) {
 		try {
 			List<String> listOfSku = getSkuList(args);
-			
+
 			pricingRules = new PricingRulesEngineImpl();
 			co = new Checkout(pricingRules);
-			
+
 			listOfSku.forEach(sku -> {
 				Item item = Products.INSTANCE.get(sku);
 				co.scan(item);
 			});
-			
+
 			co.total();
-			
+
 			printResult(listOfSku, co.getTotalCost());
-			
-			
-		} catch(IllegalArgumentException ex) {
+
+		} catch (IllegalArgumentException ex) {
 			System.out.println("Application Terminating:" + ex.getMessage());
 			System.exit(1);
-		}		
+		}
 	}
-	
+
 	private void printResult(List<String> listOfSku, double total) {
 		StringBuffer sb = new StringBuffer("SKUs Scanned: ");
 		listOfSku.forEach(sku -> {
 			sb.append(sku);
 			sb.append(", ");
 		});
-		
+
 		String result = sb.toString();
-		System.out.println(result.substring(0, result.length()-2) + " Total expected: $" + total);
+		System.out.println(result.substring(0, result.length() - 2) + " Total expected: $" + total);
 	}
+
 	/*
 	 * breaks the comma separated list into individual List items
 	 * 
@@ -86,13 +86,13 @@ class CheckoutApp {
 		while (st.hasMoreTokens()) {
 			String tkn = st.nextToken();
 			if (Products.INSTANCE.get(tkn) == null) {
-				throw new IllegalArgumentException("Invalid SKU: " + tkn);
+				throw new IllegalArgumentException("Invalid SKU: " + tkn + "\nValid SKUs are: ipd/mbp/atv/vga");
 			}
 			skuList.add(tkn);
 		}
 		return skuList;
 	}
-	
+
 	/*
 	 * Initializes the Application by loading Product data
 	 */
@@ -101,12 +101,12 @@ class CheckoutApp {
 		Item mac = new MacBookPro("mbp", "MacBook Pro", 1399.99);
 		Item atv = new AppleTV("atv", "Apple TV", 109.5);
 		Item vga = new VGAAdapter("vga", "VGA adapter", 30.0);
-		
+
 		Products.INSTANCE.add(ipad);
 		Products.INSTANCE.add(mac);
 		Products.INSTANCE.add(atv);
 		Products.INSTANCE.add(vga);
-		
+
 		return;
 	}
 }
