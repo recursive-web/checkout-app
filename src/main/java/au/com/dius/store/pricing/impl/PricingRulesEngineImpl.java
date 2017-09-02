44e767c48ffcebe6c3fc517a275b9578533e1043
abster <rajakarunanayake@gmail.com>
2017-09-02 16:54:03 +1000

    documentation

--- src/main/java/au/com/dius/store/pricing/impl/PricingRulesEngineImpl.java
+++ src/main/java/au/com/dius/store/pricing/impl/PricingRulesEngineImpl.java
@@ -7,8 +7,17 @@ import au.com.dius.store.pricing.PricingRulesEngine;
 import au.com.dius.store.promotion.ProductPromotion;
 import au.com.dius.store.promotion.ProductPromotionFactory;
 
+/*
+ * This class is injected into the Checkout class at instantiation.
+ * 
+ * It handles the calculation of the final cost of a shopping cart.
+ */
 public class PricingRulesEngineImpl implements PricingRulesEngine {
-
+	
+	/*
+	 * This implementation passes on the responsibility of calculating the cost
+	 * to a Promotion instance of each Product type. 
+	 */
 	@Override
 	public double calculateTotal() {
 		double totalCost = 0;
